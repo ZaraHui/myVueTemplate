1d81c7bba47e20997c7fc8917146882aa2b0f727
Zara <zara@ZaradeMacBook-Pro.local>
2018-10-30 11:28:14 +0800

    下载px2remLoader插件，px自动转化为rem

--- src/components/home/homeComponent1.vue
+++ src/components/home/homeComponent1.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="container">
-        This is homeComponent1
+        <h1>This is homeComponent1</h1>
+        <div>
+            <p class="title">1. 使用rem布局</p>
+            <div class="wrap">
+                <div class="box"></div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,7 +18,34 @@
     @import '../../common/style/color.scss';
 
     .container {
-        color: $text-high-color;
-        font-size: 24px;
+        h1 {
+            color: $text-base-color;
+            font-size: 50px; /*no*/
+        }
+
+        p {
+            text-align: left;
+            margin: 20px 16px; /*px自动转化成rem，如不使用rem，在样式后加 \/*no*\/ 即可*/
+            font-size: 28px; /*no*/
+        }
+
+        .wrap {
+            position: relative;
+            width: 100%;
+            height: 300px;
+            background: $high-color;
+        }
+
+        .box {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            width: 200px;
+            height: 200px;
+            background: #FFFFFF;
+            border-radius: 50%;
+            margin-top: -100px;
+            margin-left: -100px;
+        }
     }
 </style>
